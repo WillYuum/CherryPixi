b3e8f5f227c1aaf4d1e806e74d2575b90569846d
WillYum <liamshere98@gmail.com>
2024-11-30 23:52:26 +0200

    setup basic logic for button on click callback logic

--- src/SpinButton.ts
+++ src/SpinButton.ts
@@ -7,6 +7,19 @@ export class SpinButton extends Container {
         const button = Sprite.from("spin_btn_normal");
         button.anchor.set(0.5);
         this.addChild(button);
+
+        this.interactive = true;
+        this.onclick = this.handleClick;
+    }
+
+
+    public setPosition(x: number, y: number): void {
+        this.position.set(x, y);
+    }
+
+
+    private handleClick(): void {
+        console.log("Spin button clicked");
     }
 
     update(dt: number): void { }
