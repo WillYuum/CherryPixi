7474f18e89273fbfed9ddc6abcb05c33b864ef8d
WillYuum <liamshere98@gmail.com>
2025-02-13 16:50:42 +0200

    Minor optimiation to tween manager logic

--- src/TweenLogic/TweenManager.ts
+++ src/TweenLogic/TweenManager.ts
@@ -7,7 +7,7 @@ import { Tween } from "@tweenjs/tween.js";
 export class TweenManager {
     private static _instance: TweenManager;
     private tweens: Tween[] = [];
-
+    private hasTweens: boolean = false;
 
     public static getInstance(): TweenManager {
         if (!TweenManager._instance) {
@@ -17,22 +17,26 @@ export class TweenManager {
         return TweenManager._instance;
     }
 
-
     public AddTween(tween: Tween): void {
         this.tweens.push(tween);
+        this.hasTweens = true;
     }
+
 
     public RemoveTween(tween: Tween): void {
-        this.tweens.splice(this.tweens.indexOf(tween), 1);
+        const index = this.tweens.indexOf(tween);
+        if (index !== -1) {
+            this.tweens.splice(index, 1);
+            this.hasTweens = this.tweens.length > 0;
+        }
     }
 
-
     public update(deltaTime: number): void {
-        if (this.tweens.length === 0) return;
+        if (!this.hasTweens) return;
+
         for (let i = this.tweens.length - 1; i >= 0; i--) {
             const tween = this.tweens[i];
             tween.update(deltaTime);
-
         }
     }
 }
